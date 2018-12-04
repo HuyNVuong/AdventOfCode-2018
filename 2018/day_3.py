34441052197516bfb6199b7f571a7c4f4766dabf
Nguyen Huy Vuong <vuongnguyenhuy@yahoo.com>
2018-12-04 01:00:43 -0600

    day 3 part 2 completed

--- 2018/day_3.py
+++ 2018/day_3.py
@@ -36,14 +36,29 @@ print (overlapCount(mat))
 Part 2. Find the grid that 1 elve occupied but not overlap on any one else
 All the 1's left and 1 square that contains all the 1's
 """
-def findTheNiceOne(mat):
-	pass
+def findTheNiceOne(mat, allClaims):
+	naughtyList = set()
+	for claim in allClaims:
+		pos_x = int(claim[1])
+		pos_y = int(claim[2])
+		dimension = claim[3].split('x')
+		wide = int(dimension[0])
+		tall = int(dimension[1])
+		for i in range(pos_x, pos_x + wide):
+			for j in range(pos_y, pos_y + tall):
+				if (mat[i][j] != 1):
+					naughtyList.add(claim[0])
+	theNiceOne = [claim[0] for claim in allClaims if claim[0] not in naughtyList]
+	return theNiceOne
+
+print(findTheNiceOne(mat, allClaims))
 """
 Unit testing
 """
 def test(test_case):
 	allClaims = [re.findall(r"[\w']+", (line.strip())) for line in test_case]
 	mat = claim(allClaims)
+	# print(findTheNiceOne(mat, allClaims))
 	return overlapCount(mat)
 
 TEST_1 = [
