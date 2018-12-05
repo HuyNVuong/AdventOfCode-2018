50eae588d3d8b29cf9480817189db44cac332a54
Nguyen Huy Vuong <vuongnguyenhuy@yahoo.com>
2018-12-05 17:14:30 -0600

    bug fix

--- 2018/day_5.py
+++ 2018/day_5.py
@@ -2,35 +2,10 @@ import copy
 '''
 Part 1.
 String repulsive reaction. If 2 letters next to each other are the same
-but not equal (Aa not AA) then pop both of them out. Recurse until the
-string can't pop any more character
+but not equal (Aa not AA) then pop both of them out. Go left if find 1 pair
+go right otherwise.
 '''
 def react(polymer):
-    i = 0
-    length = len(polymer)
-    while i < len(polymer) - 1:
-        if (polymer[i].lower() == polymer[i + 1].lower()) and (polymer[i] != polymer[i + 1]):
-            polymer.pop(i)
-            polymer.pop(i)
-        i += 1
-
-    if length == len(polymer):
-        return len(polymer)
-    return react(polymer)
-
-
-'''
-Part 2.
-Find most efficient reaction with removing 2 character out
-Brute force by try removing each pair, would take long, but
-not break the machine since nothing would cause iteration to raise
-exponentially
-'''
-'''
-This would avoid recursion depth maxima error
-If found, go left, otherwise, go right
-'''
-def reactIterative(polymer):
     i = 0
     length = len(polymer)
     while i < len(polymer) - 1:
@@ -43,6 +18,12 @@ def reactIterative(polymer):
     return len(polymer)
 
 
+'''
+Part 2.
+Find most efficient reaction with removing 2 character out
+Brute force by try removing each pair, and try react all of them
+'''
+
 def tryAll(polymer, polymerSet):
     minLength = 1000000
     oldString = copy.deepcopy(polymer)
@@ -51,21 +32,20 @@ def tryAll(polymer, polymerSet):
         pos = c.upper()
         polymer = polymer.replace(neg, "")
         polymer = polymer.replace(pos, "")
-        curr_length = reactIterative(list(polymer))
+        curr_length = react(list(polymer))
         if(minLength > curr_length):
             minLength = curr_length
         polymer = copy.deepcopy(oldString)
     return minLength
 
-
 with open('day_5.in') as f:
     polymer = f.readline().strip()
     polymerSet = set(polymer.lower())
 
-print (reactIterative(list(polymer)))
+print (react(list(polymer)))
 print (tryAll(polymer, polymerSet))
 
 TEST = "dabAcCaCBAcCcaDA"
 
-assert reactIterative(list(TEST)) == 10
+assert react(list(TEST)) == 10
 assert tryAll(TEST, set(TEST.lower())) == 4
